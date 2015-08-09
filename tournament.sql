8ab356ccabe7b8adf94aa0c44ba0f4fae21e4766
unjo <unnjo63@hotmail.co.jp>
2015-08-10 00:31:10 +0900

    swissPairings

--- tournament.sql
+++ tournament.sql
@@ -7,19 +7,31 @@
 -- these lines here.
 
 --create table players, which has 2 column.
-CREATE TABLE players ( pname TEXT, id SERIAL );
-CREATE TABLE matches ( p1 SERIAL, p2 SERIAL, winner SERIAL );
+CREATE TABLE players ( name TEXT, id SERIAL primary key );
+CREATE TABLE matches ( winner SERIAL, loser SERIAL,
+PRIMARY KEY(winner,loser) );
 
 CREATE VIEW match_count AS
-  SELECT id, COUNT(players.id) AS games
+  SELECT id, COUNT(players.id) AS played
     FROM players, matches
-    WHERE players.id = matches.p1 OR players.id = matches.p2
+    WHERE players.id = matches.winner OR players.id = matches.loser
     GROUP BY id
-    ORDER BY games DESC;
+    ORDER BY played DESC;
+
 CREATE VIEW win_count AS
   SELECT id, COUNT(matches.winner) AS wins
-  FROM matches, players
+  FROM players, matches
   WHERE players.id = matches.winner
   GROUP BY id
   ORDER BY wins DESC;
---CREATE VIEW standings as
+
+ CREATE VIEW standings AS
+  SELECT players.id, players.name,
+  coalesce(win_count.wins,0) as wins,
+  coalesce(match_count.played,0) as played
+  FROM players
+  LEFT JOIN win_count
+  ON players.id = win_count.id
+  LEFT JOIN match_count
+  ON players.id = match_count.id
+  ORDER BY wins DESC;
